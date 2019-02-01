11799339faae0661a054eb5416c291a7c7a435f3
vadherakshay75 <vadherakshay75@gmail.com>
2019-02-01 18:07:06 +0530

    Modified Series

--- series/series.rb
+++ series/series.rb
@@ -1,5 +1,5 @@
-# Exercise 5
 require 'byebug'
+# Exercise 5
 class Series
   attr_accessor :str_array, :length_of_str, :data, :array
   def initialize(str)
@@ -12,10 +12,10 @@ class Series
 
   def slices(length_of_str)
     raise ArgumentError if length_of_str > @str.length
-    @str_array=[]
+    @str_array = []
     for i in 0..(@str.length-length_of_str) do
       @str_array << @str[i..(i + length_of_str - 1)]
     end
-    return @str_array
+    @str_array
   end
 end
