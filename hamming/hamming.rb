cc7d1ece777a92c1f65bcdaf6640719ef85be4cd
vadherakshay75 <vadherakshay75@gmail.com>
2019-02-05 16:24:18 +0530

    CR changes in hamming.rb

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,11 +2,7 @@ require 'byebug'
 # Exercise 7
 class Hamming
   def self.compute(str1, str2)
-    @str1 = str1
-    @str2 = str2
-    raise ArgumentError if @str1.length != @str2.length
-    p @str1.chars.zip(@str2.chars).count{ |st1, st2| st1 != st2 }
+    raise ArgumentError if str1.length != str2.length
+    str1.chars.zip(str2.chars).count{ |st1, st2| st1 != st2 }
   end
 end
-Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
-# 
