09d0639f652a5a4d118ec7daf47de64cc5664477
vadherakshay75 <vadherakshay75@gmail.com>
2019-02-05 16:27:21 +0530

    CR changes in word_count.rb

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,17 +1,17 @@
 require 'byebug'
 # Exercise 6
 class Phrase
-  attr_accessor :str, :str_array, :wrd, :cnt
+  attr_accessor :str, :str_array, :wrd
   def initialize(str)
     @str = str.downcase
-    @words = Hash.new(0)
     @str_array = @str.scan(/\b[\w']+\b/)
   end
 
   def word_count
-    @str_array.each do |word|
-      @words[word] += 1
+    words = Hash.new(0)
+    str_array.each do |word|
+      words[word] += 1
     end
-    p @words
+    words
   end
 end
