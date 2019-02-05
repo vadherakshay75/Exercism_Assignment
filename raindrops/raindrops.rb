d450b8e7e16499c7a9e2423cdadb0522d25d51ce
vadherakshay75 <vadherakshay75@gmail.com>
2019-02-05 16:28:39 +0530

    CR changes in raindrops.rb

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -2,7 +2,6 @@ require 'byebug'
 # Exercise 8
 class Raindrops
   def self.convert(data)
-    @data = data
     op = ''
     op += 'Pling' if data.modulo(3).zero?
     op += 'Plang' if data.modulo(5).zero?
